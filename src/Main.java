3817eac0b5f94fd6b313ba81c146501c1ede96dc
sillyEjevika <Doroteia_Vika@mail.ru>
2022-12-10 13:16:24 +0300

    задача 1-3 (решено

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // Задача №1
         System.out.println("Задача №1");
         int firstDeposit = 15000;
@@ -10,25 +9,35 @@ public class Main {
             total = total + total / 100;
             total = total + firstDeposit;
             month = month + 1;
-            System.out.println("Месяц " +  month + " , сумма накоплений равна " + total + " рублей");
+            System.out.println("Месяц " + month + " , сумма накоплений равна " + total + " рублей");
         }
         System.out.println("Итого накоплений " + total);
         System.out.println("Итого месяцев " + month);
-        System.out.println("Итого лет " + month/12);
+        System.out.println("Итого лет " + month / 12);
 
         // Задача №2
         System.out.println("Задача №2");
-        int i = 1;
-        while (i<10) {
+        int i = 0;
+        while (i < 10) {
+            i = i + 1;
             System.out.print(" " + i);
         }
-        int k = 1;
-        for (int k = 10; k >= 1; a--) ;{
-            System.out.print(" " + a);
+        System.out.println();
+        for (int gg = 10; gg >= 1; gg--){
+                System.out.print(" " + gg);
         }
-            System.out.println(a);
-            System.out.println(i);
-        }
+        System.out.println();
 
+        // Задача №3
+        System.out.println("Задача №3");
+        int population = 12_000_000;
+        int bornPer1000 = 17;
+        int diePer1000 = 8;
+        int dif = bornPer1000 - diePer1000;
+           for (int f = 1; f <= 10; f ++){
+               population += population * dif/1000;
+            System.out.println ("Год " + f + " ,численность населения составляет " + population + " человек");
+        }
+    }
 }
 
