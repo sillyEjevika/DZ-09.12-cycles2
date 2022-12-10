53fbf71c74481e0562b422518e30b2a383e20a7c
sillyEjevika <Doroteia_Vika@mail.ru>
2022-12-10 15:40:25 +0300

    задача 1-37 (решено

--- src/Main.java
+++ src/Main.java
@@ -23,8 +23,8 @@ public class Main {
             System.out.print(" " + i);
         }
         System.out.println();
-        for (int gg = 10; gg >= 1; gg--){
-                System.out.print(" " + gg);
+        for (int gg = 10; gg >= 1; gg--) {
+            System.out.print(" " + gg);
         }
         System.out.println();
 
@@ -34,10 +34,60 @@ public class Main {
         int bornPer1000 = 17;
         int diePer1000 = 8;
         int dif = bornPer1000 - diePer1000;
-           for (int f = 1; f <= 10; f ++){
-               population += population * dif/1000;
-            System.out.println ("Год " + f + " ,численность населения составляет " + population + " человек");
+        for (int f = 1; f <= 10; f++) {
+            population += population * dif / 1000;
+            System.out.println("Год " + f + ", численность населения составляет " + population + " человек");
         }
+        // Задача №4
+        System.out.println("Задача №4");
+        int deposit = 15000;
+        int total1 = 0;
+        int month1 = 0;
+        int plusPerMonth = 7;
+        while (total1 <= 12_000_000) {
+            total1 = total1 + total1 * plusPerMonth / 100;
+            total1 = total1 + deposit;
+            month1 = month1 + 1;
+            System.out.println("Месяц " + month1 + " , сумма накоплений равна " + total1 + " рублей");
+        }
+        System.out.println(total1);
+        System.out.println(month1);
+
+        // Задача 5
+        System.out.println("Задача №5");
+        int deposit5 = 15000;
+        int total5 = 0;
+        int month5 = 0;
+        int plusPerMonth5 = 7;
+        while (total5 <= 12_000_000) {
+            total5 = total5 + total5 * plusPerMonth5 / 100;
+            total5 = total5 + deposit5;
+            month5 = month5 + 1;
+            if (month5 % 6 == 0) {
+                System.out.println("Месяц " + month5 + " , сумма накоплений равна " + total5 + " рублей");
+            }
+        }
+        // Задача 6
+        System.out.println("Задача №6");
+        int dep = 15000;
+        int total11 = 0;
+        int m = 0;
+        for (int year =0; year <= 108; year ++ ) {
+            total11 = total11  + total11  * 7/100;
+            total11  = total11  + dep;
+            m = m + 1;
+            if (m % 6 == 0) {
+                System.out.println("Месяц " + m + " , сумма накоплений равна " + total11 + " рублей");
+            }
+        }
+        // Задача 7
+        System.out.println("Задача №7");
+        for (int days = 2; days <=31; days = days+7) {
+            System.out.println ("Сегодня пятница, " + days + "-е число. Необходимо подготовить отчет");
+        }
+        // Задача 8
+        System.out.println("Задача №8");
+
+
     }
 }
-
